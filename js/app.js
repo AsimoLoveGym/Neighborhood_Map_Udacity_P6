8efd29cf2515d48edf1fa59a345e533be06b16be
AsimoLoveGym <xiazhuangz@gmail.com>
2016-08-30 16:14:22 -0500

    Code Quality checked us JSHint, JSBeautify, HTML and CSS checker

--- js/app.js
+++ js/app.js
@@ -6,81 +6,74 @@
  * {Udacity FEND NanoDegree - Neighborhood Map Project} can not be copied and/or distributed without the express
  * permission of {name}
  *******************************************************/
-
-
-
-
 // Credit to Udacity Coach MarkN https://discussions.udacity.com/t/how-to-make-ajax-request-to-yelp-api/13699/4
-
 /**
  * Generates a random number and returns it as a string for OAuthentication
  * @return {string}
  */
-
-
- function yelpApi(selectedYelpId){
-  function nonce_generate() {
-    return (Math.floor(Math.random() * 1e12).toString());
-  }
+function yelpApi(selectedYelpId) {
+    function nonce_generate() {
+        return (Math.floor(Math.random() * 1e12).toString());
+    }
 
     var YELP_BASE_URL = 'http://api.yelp.com/v2/';
-    var yelp_url = YELP_BASE_URL + 'business/'+ selectedYelpId;
+    var yelp_url = YELP_BASE_URL + 'business/' + selectedYelpId;
 
-  // to define YELP_KEY, YELP_TOKEN
-      var YELP_KEY = 'jT35AXIRI8LXdlEnJOG_FQ';
-      var YELP_TOKEN = '72nsiEPyM4q-TMXsFD9qiDCktKtyc0Io';
-      var YELP_KEY_SECRET = 'fVwEq4DqL2OQLpLkWoZOm2Ra1QA';
-      var YELP_TOKEN_SECRET = 'g0rjtar7cJ6-vHyAFAcknCK-914';
+    // to define YELP_KEY, YELP_TOKEN
+    var YELP_KEY = 'jT35AXIRI8LXdlEnJOG_FQ';
+    var YELP_TOKEN = '72nsiEPyM4q-TMXsFD9qiDCktKtyc0Io';
+    var YELP_KEY_SECRET = 'fVwEq4DqL2OQLpLkWoZOm2Ra1QA';
+    var YELP_TOKEN_SECRET = 'g0rjtar7cJ6-vHyAFAcknCK-914';
 
-      var parameters = {
+    var parameters = {
         oauth_consumer_key: YELP_KEY,
         oauth_token: YELP_TOKEN,
         oauth_nonce: nonce_generate(),
-        oauth_timestamp: Math.floor(Date.now()/1000),
+        oauth_timestamp: Math.floor(Date.now() / 1000),
         oauth_signature_method: 'HMAC-SHA1',
-        oauth_version : '1.0',
-        callback: 'cb'              // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
-      };
+        oauth_version: '1.0',
+        callback: 'cb' // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
+    };
 
-      var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, YELP_KEY_SECRET, YELP_TOKEN_SECRET);
-      parameters.oauth_signature = encodedSignature;
+    var encodedSignature = oauthSignature.generate('GET', yelp_url, parameters, YELP_KEY_SECRET, YELP_TOKEN_SECRET);
+    parameters.oauth_signature = encodedSignature;
 
-      var settings = {
+    var settings = {
         url: yelp_url,
         data: parameters,
-        cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
+        cache: true, // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
         dataType: 'jsonp',
         success: function(results) {
-          // Do stuff with results
-          var content = '';
-          content = '<h3>' + results.name + '</h3>' +
-            '<span><strong>Rating: </strong></span>' +
-            '<img src=' + results.rating_img_url +'>' +
-            '<span><strong>  ' + results.rating + '</strong></span>' +
-            '<p><strong>Reviews: </strong>' +
-            '<span>  ' + results.review_count + ' </span></p>' +
-            '<p>' + results.categories[0][0] + '</p>';
-          if(results.categories[1]){
-            content += '<p>' + results.categories[1][0] + '</p>';
-          }
-          content += '<p>' + results.location.display_address[0] + results.location.display_address[1] + '</p>' +
-            '<p>' + results.display_phone + '</p>';
-          if(results.image_url){
-            content += '<img src=' + results.image_url +'>';
-          }
+            // Do stuff with results
+            var content = '';
+            content = '<h3>' + results.name + '</h3>' +
+                '<span><strong>Rating: </strong></span>' +
+                '<img src=' + results.rating_img_url + '>' +
+                '<span><strong>  ' + results.rating + '</strong></span>' +
+                '<p><strong>Reviews: </strong>' +
+                '<span>  ' + results.review_count + ' </span></p>' +
+                '<p>' + results.categories[0][0] + '</p>';
+            if (results.categories[1]) {
+                content += '<p>' + results.categories[1][0] + '</p>';
+            }
+            content += '<p>' + results.location.display_address[0] + results.location.display_address[1] + '</p>' +
+                '<p>' + results.display_phone + '</p>';
+            if (results.image_url) {
+                content += '<img src=' + results.image_url + '>';
+            }
 
-          largeInfowindow.setContent(content);
+            largeInfowindow.setContent(content);
         },
         error: function(xhr, status, error) {
-          // Do stuff on fail
-          content = "<img src='image/error.png'>" +
-          "<p>" + "An AJAX error occured: " + status + "</p>" +
-          "<p>" + "Error: " + error + "</p>" +
-          "<p>" + "Error detail: " + xhr.responseText + "</p>";
-          largeInfowindow.setContent(content);
+            // Do stuff on fail
+            content = "<img src='image/error.png'>" +
+                "<p>" + "An AJAX error occured: " + status + "</p>" +
+                "<p>" + "Error: " + error + "</p>" +
+                "<p>" + "Error detail: " + xhr.responseText + "</p>";
+            largeInfowindow.setContent(content);
         }
-      };
+    };
 
-      // Send AJAX query via jQuery library.
-      $.ajax(settings);
- }
+    // Send AJAX query via jQuery library.
+    $.ajax(settings);
+}
--- js/map.js
+++ js/map.js
@@ -6,113 +6,130 @@
  * {Udacity FEND NanoDegree - Neighborhood Map Project} can not be copied and/or distributed without the express
  * permission of {name}
  *******************************************************/
-
 var map;
 var largeInfowindow;
 
 // Constructor function for each location
-var Point = function(data){
-  this.title = data.title;
-  this.location = data.location;
-  this.yelpId = data.yelpId;
-}
+var Point = function(data) {
+    this.title = data.title;
+    this.location = data.location;
+    this.yelpId = data.yelpId;
+};
 
 // ******************* Model Below ******************
 
-var locations = [
-      {
-        title: 'Sakanaya Restaurant, Japanese restaurant',
-        location: {lat: 40.110155, lng: -88.233122},
-        yelpId: 'sakanaya-restaurant-champaign'
-      },
-      {
-        title: 'Le Peep, My favorite brunch restaurant',
-        location: {lat: 40.087095, lng: -88.247949},
-        yelpId: 'le-peep-restaurant-champaign'
-      },
-      {
-        title: 'The Art Theater',
-        location: {lat: 40.118571, lng: -88.244699},
-        yelpId: 'the-art-theater-champaign'
-      },
-      {
-        title: 'The Red Lion',
-        location: {lat: 40.109972, lng: -88.235615},
-        yelpId: 'red-lion-champaign'
-      },
-      {
-        title: 'Crystal Lake Park Family Aquatic Center',
-        location: {lat: 40.12518, lng: -88.209342},
-        yelpId: 'crystal-lake-family-aquatic-center-urbana'
-      },
-      {
-        title: 'Activities and Recreation Center',
-        location: {lat: 40.10083, lng: -88.235555},
-        yelpId: 'activities-and-recreation-center-arc-champaign'
-      },
-      {
-        title: 'Lai Lai Wok, my favorite Chinese restaurant',
-        location: {lat: 40.110455, lng: -88.233305},
-        yelpId: 'lai-lai-wok-champaign'
-      }
-    ];
+var locations = [{
+    title: 'Sakanaya Restaurant, Japanese restaurant',
+    location: {
+        lat: 40.110155,
+        lng: -88.233122
+    },
+    yelpId: 'sakanaya-restaurant-champaign'
+}, {
+    title: 'Le Peep, My favorite brunch restaurant',
+    location: {
+        lat: 40.087095,
+        lng: -88.247949
+    },
+    yelpId: 'le-peep-restaurant-champaign'
+}, {
+    title: 'The Art Theater',
+    location: {
+        lat: 40.118571,
+        lng: -88.244699
+    },
+    yelpId: 'the-art-theater-champaign'
+}, {
+    title: 'The Red Lion',
+    location: {
+        lat: 40.109972,
+        lng: -88.235615
+    },
+    yelpId: 'red-lion-champaign'
+}, {
+    title: 'Crystal Lake Park Family Aquatic Center',
+    location: {
+        lat: 40.12518,
+        lng: -88.209342
+    },
+    yelpId: 'crystal-lake-family-aquatic-center-urbana'
+}, {
+    title: 'Activities and Recreation Center',
+    location: {
+        lat: 40.10083,
+        lng: -88.235555
+    },
+    yelpId: 'activities-and-recreation-center-arc-champaign'
+}, {
+    title: 'Lai Lai Wok, my favorite Chinese restaurant',
+    location: {
+        lat: 40.110455,
+        lng: -88.233305
+    },
+    yelpId: 'lai-lai-wok-champaign'
+}];
 
 // ******************* Map Initiation Below ******************
 
-function initMap(){
-  map = new google.maps.Map(document.getElementById('map'),{
-          center: {lat: 40.115634,lng: -88.236542},
-          zoom: 13
-        });
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 40.115634,
+            lng: -88.236542
+        },
+        zoom: 13
+    });
 
-  largeInfowindow = new google.maps.InfoWindow();
+    largeInfowindow = new google.maps.InfoWindow();
 
-// To solve the scoping issue raised
-  ko.applyBindings(new ViewModel());
+    // To solve the scoping issue raised
+    ko.applyBindings(new ViewModel());
 }
 
 // ******************* View Model Below ******************
 
-var ViewModel = function(){
+var ViewModel = function() {
     var self = this;
 
-    this.lcfilter= ko.observable("");
+    this.lcfilter = ko.observable("");
 
     this.locations = ko.observableArray();
 
-    this.setInfoWindow = function(){
-      // this should be the selected location
+    this.setInfoWindow = function() {
 
-      // Reset all animation to null
-      for(var i = 0; i < self.locations().length; i++) {
-        self.locations()[i].marker.setAnimation(null);
-      }
+        self.markerAnimation(this.marker, this.marker.yelpId);
 
-      yelpApi(this.marker.yelpId);
-      largeInfowindow.open(map, this.marker);
-      // populateInfoWindow(this.marker, largeInfowindow);
+    };
 
-      this.marker.setAnimation(google.maps.Animation.BOUNCE);
-      largeInfowindow.addListener('closeclick',function(){
-         this.marker.setAnimation(null);
-      });
+    self.markerAnimation = function(marker, yelpId) {
+        for (var i = 0; i < self.locations().length; i++) {
+            self.locations()[i].marker.setAnimation(null);
+        }
+
+        yelpApi(yelpId);
+        largeInfowindow.open(map, marker);
+
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        largeInfowindow.addListener('closeclick', function() {
+            marker.setAnimation(null);
+        });
     };
 
     // Push all the initial hard coded location into observableArray.
-    locations.forEach(function(locationPoint){
-      self.locations().push(new Point(locationPoint));
+    locations.forEach(function(locationPoint) {
+        self.locations().push(new Point(locationPoint));
     });
 
     // Filter function for filtering list view
     this.filteredItems = ko.computed(function() {
         var filterWords = this.lcfilter().toLowerCase();
         if (!filterWords) {
-          // Reset the marker for each location
-            for(var i = 0; i < this.locations().length; i++) {
-              // Cause the define and set of marker is after lcfilter function here,
-              // need to check the marker properties for locations existed or not.
-              if(this.locations()[i].marker)
-              this.locations()[i].marker.setVisible(true);
+            // Reset the marker for each location
+            for (var i = 0; i < this.locations().length; i++) {
+                // Cause the define and set of marker is after lcfilter function here,
+                // need to check the marker properties for locations existed or not.
+                if (this.locations()[i].marker)
+                    this.locations()[i].marker.setVisible(true);
             }
             return this.locations();
         } else {
@@ -122,73 +139,55 @@ var ViewModel = function(){
                 // return stringStartsWith(item.title.toLowerCase(), filterWords);
 
                 // filter mechanism 2: as long as the location's name includes the input string, show it
-                if(item.title.toLowerCase().indexOf(filterWords) > -1) {
-                  // for filtered locations, mark them on the map and animated the marker once.
-                  item.marker.setVisible(true);
-                  item.marker.setAnimation(google.maps.Animation.BOUNCE);
-                  // 750 ms is the time needed for the marker to finish one bounce.
-                  setTimeout(function(){item.marker.setAnimation(null);},750);
-                  return true;
+                if (item.title.toLowerCase().indexOf(filterWords) > -1) {
+                    // for filtered locations, mark them on the map and animated the marker once.
+                    item.marker.setVisible(true);
+                    item.marker.setAnimation(google.maps.Animation.BOUNCE);
+                    // 750 ms is the time needed for the marker to finish one bounce.
+                    setTimeout(function() {
+                        item.marker.setAnimation(null);
+                    }, 750);
+                    return true;
                 } else {
                     item.marker.setVisible(false);
                     return false;
                 }
             });
         }
-    },this);
+    }, this);
 
-    for(var i = 0; i < this.locations().length; i++) {
-      // initialize the properties for each location
+    for (var i = 0; i < this.locations().length; i++) {
+        // initialize the properties for each location
         var position = this.locations()[i].location;
         var title = this.locations()[i].title;
         var marker = new google.maps.Marker({
-          position: position,
-          map: map,
-          title: title,
-          animation: google.maps.Animation.DROP,
-          id: i
+            position: position,
+            map: map,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
         });
         this.locations()[i].marker = marker;
         this.locations()[i].marker.yelpId = this.locations()[i].yelpId;
-        this.locations()[i].marker.addListener('click', function(){
-          // disguish the self, self is only used for viewmodel.
-          var that = this;
-
-          for(var i = 0; i < self.locations().length; i++) {
-            self.locations()[i].marker.setAnimation(null);
-          }
-
-          yelpApi(this.yelpId);
-          largeInfowindow.open(map, this);
-          // populateInfoWindow(this, largeInfowindow);
-
-          this.setAnimation(google.maps.Animation.BOUNCE);
-          largeInfowindow.addListener('closeclick',function(){
-            that.setAnimation(null);
-          });
-
+        this.locations()[i].marker.addListener('click', function() {
+            self.markerAnimation(this, this.yelpId);
         });
+        // this.locations()[i].marker.addListener('click', self.markerAnimation(this, this.yelpId));
     }
 
-    // function populateInfoWindow(marker, infowindow) {
-    //   if (infowindow.marker != marker) {
-    //     infowindow.marker = marker;
-    //     infowindow.open(map, marker);
-    //   }
-    // }
-}
+};
 
 // only be used for filter mechanism 1
-var stringStartsWith = function (string, startsWith) {
+var stringStartsWith = function(string, startsWith) {
     string = string || "";
     if (startsWith.length > string.length)
         return false;
     return string.substring(0, startsWith.length) === startsWith;
-}
+};
 
 // Navigation toggle function for mobile
 function navToggle() {
-    var x = document.getElementsByTagName("BODY")[0];;
+    var x = document.getElementsByTagName("BODY")[0];
     if (x.className === "") {
         x.className += "header-visible";
     } else {
